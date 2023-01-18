d8d71faa3dd93b5e0dccae8de7b0646b265d5cb9
Kierre Willoughby <kwilloughy1@uco.edu>
2023-01-18 16:53:27 -0600

    close to getting things working

--- src/Viewscreen/View/debtPage_Screen.js
+++ src/Viewscreen/View/debtPage_Screen.js
@@ -3,7 +3,7 @@ import CustomNavbar from "../../Model/Components/navbar";
 import { UserAuth } from "../../Controller/Auth";
 import { db } from '../../firebase';
 import * as firebase from 'firebase/app';
-import { query, collection, onSnapshot, QuerySnapshot, where } from 'firebase/firestore';
+import { query, collection, onSnapshot, QuerySnapshot, where, getDocs } from 'firebase/firestore';
 import Debt from '../../Model/debt';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
@@ -11,75 +11,119 @@ const DebtPageScreen = () => {
 
 
   const auth = getAuth();
+  const currentUser = auth.currentUser
   const collectionRef = collection(db, 'Users');
   const [people, setPeople] = useState([]);
   const [debts, setDebts] = useState([]);
   const [current, setCurrent] = useState({});
-  const [docID, setDocId] = useState({});
-  const [uid, setUid] = useState({});
+  let docID = useRef({});
+  let uid = useRef({});
+
 
   onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      console.log(uid)
-      setUid(uid)
-      // ...
+    if (!user) {
+      console.log("no user")
     } else {
       // User is signed out
       // ...
     }
   });
 
-  console.log(uid)
+  console.log(auth)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser);
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
 
 
   useEffect(() => {
-    //let uid = user.id ? uid.toString() : '';
-    let usersArr = []
-    let currentUser;
-    const q = query(collection(db, 'Users'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        usersArr.push({ ...doc.data(), id: doc.id })
-      });
-    })
-
-    console.log(usersArr);
-    //console.log(uid)
-
-    if (uid === undefined) {
-      console.log('not ready')
-    }else{
-      usersArr.forEach((doc) => {
-        if (doc.uid === uid) {
-          setDocId(doc.id)
-        }
-      })
-    }
-
-    console.log(docID)
-
-    console.log(currentUser)
-    if (uid === {}) {
-      console.log('Not Ready')
-    } else {
-      const q2 = query(collection(collectionRef,
-         where('uid', '==', uid), 'Debts'));
-      const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
-        let debtsArr = []
-        querySnapshot.forEach((doc) => {
-          debtsArr.push({ ...doc.data(), id: doc.id })
-        });
-        setDebts(debtsArr)
-
-      })
-    }
-    return () => unsubscribe()
+    //if (!user) {
+      //console.log("no user")
+    //} else {
+      const getData = async () => {
+        const q = query(collection(db, 'Users'))
+        const snapshot = await getDocs(q)
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(), id: doc.id
+        }))
+        data.map(async (elem) => {
+          const q2 = query(collection(db, `Users/${elem.id}/Debts`))
+          const debtList = await getDocs(q2)
+          const debtInfo = debtList.docs.map((doc) => ({
+            ...doc.data(), id: doc.id
+          }))
+          console.log(debtInfo);
+        })
+      }
+      getData()
+    //}
   }, [])
 
-  console.log(debts)
+  //   let usersArr = []
+  //   console.log(uid)
+  //   const q = query(collection(db, 'Users'))
+  //   //where('uid', "==", uid.current));
+  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //       usersArr.push({ ...doc.data(), id: doc.id })
+  //     });
+  //     setDebts(usersArr)
+  //   })
+
+  //   return () => unsubscribe()
+  // }, [])
+
+  // debts.forEach(function (item) {
+  //   if (item.uid == uid.current) {
+  //     docID.current = item.id;
+  //   }
+  // })
+
+  // console.log(docID);
+
+  // // console.log(docID)
+
+  // // console.log(currentUser)
+  // // if (uid === {}) {
+  // //   console.log('Not Ready')
+  // // } else {
+  // //   const q2 = query(collection(collectionRef,
+  // //      "YIKUfcd1GavutozVeNP4", 'Debts'));
+  // //   const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
+  // //     let debtsArr = []
+  // //     querySnapshot.forEach((doc) => {
+  // //       debtsArr.push({ ...doc.data(), id: doc.id })
+  // //     });
+  // //     setDebts(debtsArr)
+
+  // //   })
+  // // }
+
+  // useEffect(() => {
+  //   let debtsArr = []
+  //   const q2 = query(collection(collectionRef,
+  //     "YIKUfcd1GavutozVeNP4", 'Debts'));
+  //   const unsubscribe = onSnapshot(q2, (querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //       debtsArr.push({ ...doc.data(), id: doc.id })
+  //     });
+  //     setDebts(debtsArr)
+  //   })
+
+
+  //      return () => unsubscribe()
+  //    }, [])
+
+
+
+  //   console.log(debts)
 
   return (
     <div className="App">
