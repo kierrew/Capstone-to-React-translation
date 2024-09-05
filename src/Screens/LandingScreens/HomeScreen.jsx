70c2146ad24c7bc1ad654e2fcc1ba5bd89033661
Kierre Willoughby <kierre.willoughby@anheuser-busch.com>
2024-09-05 12:14:14 -0500

    implemented FOntawesome and updated some icons

--- src/Screens/LandingScreens/HomeScreen.jsx
+++ src/Screens/LandingScreens/HomeScreen.jsx
@@ -1,4 +1,11 @@
-
+import {
+	faAddressBook,
+	faBuildingColumns, faClipboard, faCreditCard, faDollarSign, faFolder,
+	faMoneyBillWave,
+	faScrewdriverWrench,
+	faUser, faWallet
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Button } from 'react-bootstrap';
@@ -19,9 +26,7 @@ const HomeScreen = () => {
 			<div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '4rem', justifyItems: 'center'}}>
 				<div>
 				<Button onClick={() => handleNavigation('/accounts')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
-  <path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z"/>
-</svg>
+				<FontAwesomeIcon icon={faBuildingColumns} size='5x'/>
 				</Button>
 				<h4>
 Accounts
@@ -29,10 +34,7 @@ Accounts
 				</div>
 				<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
 				<Button onClick={() => handleNavigation('/templates')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
-  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
-  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
-</svg>
+				<FontAwesomeIcon icon={faClipboard} size='5x' />
 				</Button>
 				<h4>
 Budget Templates
@@ -50,10 +52,7 @@ Currency	Exchange
 </div>
 <div>
 	<Button onClick={() => handleNavigation('/debts')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-		<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-credit-card" viewBox="0 0 16 16">
-  	<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
-  	<path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-		</svg>
+	<FontAwesomeIcon icon={faCreditCard} size='5x' />
 	</Button>
 	<h4>
 		Debts
@@ -61,10 +60,7 @@ Currency	Exchange
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/profile')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-</svg>
+				<FontAwesomeIcon icon={faUser} size='5x' />
 				</Button>
 				<h4>
 My Profile
@@ -72,9 +68,7 @@ My Profile
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/wallet')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
-  <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z"/>
-</svg>
+				<FontAwesomeIcon icon={faWallet} size='5x' />
 				</Button>
 				<h4>
 			My Wallet
@@ -82,9 +76,7 @@ My Profile
 			</div>
 			<div>
 				<Button onClick={() => handleNavigation('/plans')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-</svg>
+				<FontAwesomeIcon icon={faFolder} size='5x'/>
 				</Button>
 				<h4>
 Plans
@@ -92,9 +84,7 @@ Plans
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/savings')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
-  <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
-</svg>
+				<FontAwesomeIcon icon={faDollarSign} size='5x' />
 				</Button>
 				<h4>
 Savings
@@ -102,9 +92,7 @@ Savings
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/tools')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
-  <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3q0-.405-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708M3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"/>
-</svg>			
+				<FontAwesomeIcon icon={faScrewdriverWrench} size='5x'/>			
 				</Button>
 				<h4>
 Tools
@@ -112,10 +100,7 @@ Tools
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/transactions')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
-  <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
-  <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2z"/>
-</svg>
+				<FontAwesomeIcon icon={faMoneyBillWave} size='5x' />
 				</Button>
 <h4>
 
@@ -124,9 +109,7 @@ Transactions
 </div>
 <div>
 				<Button onClick={() => handleNavigation('/users')} variant='outline-dark' style={{display: 'flex', flexDirection: 'column', alignItems: 'center', width: '8rem', height: '8rem'}}>
-				<svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
-  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/>
-</svg>
+				<FontAwesomeIcon icon={faAddressBook} size='5x' />
 				</Button>
 <h4>
 User List
